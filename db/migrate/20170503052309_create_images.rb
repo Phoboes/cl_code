ebe297e3c28b6ca7cf2e22d542f8d05815d49b0c
Kane <phoboes@live.com>
2017-05-03 15:53:50 +1000

    first commit

---
+++ db/migrate/20170503052309_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :images do |t|
+      t.text :title
+      t.text :link
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
